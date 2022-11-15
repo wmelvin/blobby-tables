5075b6d7a04d8ed967b8bddbf1b2e18fc7881c33
Bill Melvin <bill@billmelvin.com>
2022-11-14 23:08:46 -0500

    Initial work on app configuration and creating Azure resources.

--- BlobbyTables/BlobbyConsole/Program.cs
+++ BlobbyTables/BlobbyConsole/Program.cs
@@ -1,7 +1,53 @@
-﻿internal class Program
+﻿
+internal class Program
 {
+
+    // TODO: Figure out the right way to read configuration, including locally
+    // stored secrets that are outside the project source tree, in a C#
+    // Console app. Configuration seems to be a big topic in .NET with lots
+    // of interfaces, classes, extension methods, providers, and such; and
+    // much of the information in the docs and blogs is about configuring
+    // web apps, not console apps. I couldn't get UserSecrets working here.
+    //
+    // For now, this...
+
+    private static Dictionary<string, string> LocalConfig()
+    {
+        var settings = new Dictionary<string, string>();
+
+        var fileName = Path.Join(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            "KeepLocal", "blobby-config.txt"
+        );
+        // TODO: Would want to make sure the file exists it this was a keeper,
+        // but it's just a temporary demo thing.
+
+        char[] trimmy= { ' ', '"', '\'' };
+        var reader = new StreamReader(fileName);
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            if (!string.IsNullOrEmpty(line) && line.Contains('='))
+            {
+                string[] a = line.Split("=");
+                // TODO: Is there a slicing syntax to take everything after
+                // the first '=' in the case of a setting with a '=' in the
+                // value (like a connection string)? The length will be > 2.
+                if (a.Length == 2)
+                {
+                    settings[a[0].Trim()] = a[1].Trim(trimmy);
+                }
+            }
+        }
+        reader.Close();
+        return settings;
+    }
     private static void Main(string[] args)
     {
+        var localConfig = LocalConfig();
+        localConfig.TryGetValue("ContainerKey", out string? containerKey);
+        Console.WriteLine($"ContainerKey=\"{containerKey}\"");
+
         Console.WriteLine("Where's the BLOB?");
     }
 }
--- BlobbyTables/azure-resources.ps1
+++ BlobbyTables/azure-resources.ps1
@@ -1,1 +1,61 @@
-﻿# More code, please.
+﻿# ----------------------------------------------------------------------
+# PowerShell script with steps to create resources using the Azure CLI.
+#
+# ----------------------------------------------------------------------
+
+# az login
+
+# az account set -s $SUBSCRIPTION
+
+
+# # ----------------------------------------------------------------------
+# # -- Get key variables from file in local encrypted folder.
+ 
+# $keysFile = "$env:UserProfile\KeepLocal\blobby-settings"
+
+# # -- Source the file to set the vars.
+# . $keysFile
+
+# function CheckVarSet ([string] $varName) {
+#     $val = Get-Variable -Name $varName -ValueOnly -ErrorAction:Ignore
+#     if (0 -eq $val.Length) {
+#       Write-Host "ERROR: '$varName' not set in '$keysFile'."
+#       Exit 1
+#     }
+# }
+
+# CheckVarSet "AdminUser"
+
+
+# ----------------------------------------------------------------------
+# -- Assign vars for script.
+
+$baseName = "demo04"
+$rgName = "${baseName}-rg"
+$location = "eastus"
+$storageAcctName = "${baseName}storage"
+
+# -- Create the Resource Group.
+az group create -n $rgName -l $location
+
+# -- Create the Storage Account.
+#    https://docs.microsoft.com/en-us/cli/azure/storage/account?view=azure-cli-latest#az-storage-account-create
+
+az storage account create `
+    -n $storageAcctName `
+    -l $location `
+    -g $rgName `
+    --sku Standard_LRS
+
+
+# -- Get the storage account key.
+#    Example found in Microsoft Docs: "Mount a file share to a Python function app - Azure CLI"
+#    https://docs.microsoft.com/en-us/azure/azure-functions/scripts/functions-cli-mount-files-storage-linux
+
+$storageKey = $(az storage account keys list -g $rgName -n $storageAcctName --query '[0].value' -o tsv)
+
+
+# -- Create storage containers.
+#    https://docs.microsoft.com/en-us/cli/azure/storage/container?view=azure-cli-latest#az-storage-container-create
+
+az storage container create --account-key $storageKey --account-name $storageAcctName -n "blobby1"
